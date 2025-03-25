10361fbef4ad2c79841035a380946c6b6cd518ea
nutsa30 <nutsarogava30@gmail.com>
2025-03-25 19:40:41 +0400

    Add files via upload

--- shopItems/ShopItems.tsx
+++ shopItems/ShopItems.tsx
@@ -14,7 +14,7 @@ const ShopItems = () => {
 
 
         <div className={styles.shopItems}>
-            <img   className={styles.shopImg1} src="/shared/desktop/image-category-thumbnail-headphones.png" alt="Headphones"  />
+            <img   className={styles.shopImg1} src="public/shared/desktop/image-category-thumbnail-headphones.png" alt="Headphones"  />
             <h2  >HEADPHONES</h2>
             <button 
             onClick={()=>
@@ -30,7 +30,7 @@ const ShopItems = () => {
 
 
         <div className={styles.shopItems}>
-            <img className={styles.shopImg2} src="/shared/desktop/image-category-thumbnail-speakers.png" alt="Speakers" />
+            <img className={styles.shopImg2} src="public/shared/desktop/image-category-thumbnail-speakers.png" alt="Speakers" />
             <h2>SPEAKERS</h2>
             <button
             onClick={()=>
@@ -44,7 +44,7 @@ const ShopItems = () => {
 
 
         <div className={styles.shopItems}>
-            <img className={styles.shopImg3} src="/shared/desktop/image-category-thumbnail-earphones.png" alt="Earphones" />
+            <img className={styles.shopImg3} src="public/shared/desktop/image-category-thumbnail-earphones.png" alt="Earphones" />
             <h2>EARPHONES</h2>
             <button
             onClick={()=>
